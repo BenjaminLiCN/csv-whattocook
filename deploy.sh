961efeb4a308b476f9275ad375d9ce12410b6c8c
BenjaminLiCN <li.benjamincn@gmail.com>
2019-04-14 00:31:08 +1000

    CI/CD finalisation

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,7 @@
 echo "build successfully, deploying to the server..."
 cd /var/lib/jenkins/workspace/whattocook
 echo "kill the existing process"
-
 pid=`ps -ef|grep -e root|grep -e SNAPSHOT|awk '{print $2}'`
-
 if [[ "$pid" = "" ]]
 then
   echo "no process exists... move on to mvn compiling"
@@ -17,5 +15,5 @@ echo "generating new target..."
 mvn clean package -Dmaven.test.skip=true
 cd target
 echo "run a new process"
-nohup java -jar benjamin-0.0.1-SNAPSHOT.jar > runLog.file
+nohup java -jar benjamin-0.0.1-SNAPSHOT.jar > runLog.file 2>&1 &
 echo "back end deployment completed!"
